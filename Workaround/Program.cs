b3aaaf113606494250c792b888ecc90c4c30f721
Grafiker <Grafiker@DESKTOP-FP91Q05>
2024-02-11 18:04:49 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,131 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();
+
+            Vatandas vatandas1 = new Vatandas();   // instance
+
+            SelamVer("Engin");
+            SelamVer("Ahmet");
+            SelamVer("Ayşe");
+            SelamVer();
+
+            int sonuc = Topla(6, 58);
+
+            // Diziler - Arrays
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Kerem";
+            string ogrenci3 = "Berkay";
+
+            Console.WriteLine(ogrenci1);
+            Console.WriteLine(ogrenci2);
+            Console.WriteLine(ogrenci3);
+
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Kerem";
+            ogrenciler[2] = "Berkay";
+            //ogrenciler[3] = "İlker";   3 elemanlı bir diziye 4. elemanı ekleyemeyiz, hata alırız.
+
+            ogrenciler = new string[4];   //Bu şekilde yazarsak, 4 elemanlı 3. elemanı İlker olan diğer elemanları boş olan yeni bir dizi oluşturmuş oluruz.
+            ogrenciler[3] = "İlker";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            int sayi1 = 10;
+            int sayi2 = 20;
+            sayi2 = sayi1;
+            sayi1 = 30;
+            // sayi2 ??     10
+
+            Person person1 = new Person();   //Bellekte bir yer açtık
+            person1.FirstName = "Engin";
+            person1.LastName = "Demiroğ";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 123;
+
+            //Person person1 = new Person { FirstName = "Engin" };  Böyle de yazılabilir
+
+            Person person2 = new Person();    //Bellekte başka bir yer açtık
+            person2.FirstName = "Murat";
+
+            foreach (string sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            List<string> yeniSehirler1 = new List<string> { "Ankara 1", "İstanbul 1", "İzmir 1" };
+            yeniSehirler1.Add("Adana 1");
+
+            foreach (var sehir in yeniSehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            
+
+
+        }
+
+        static void SelamVer(string isim = "isimsiz")
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam: " + sonuc);
+            return sonuc;
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000;   //db'den gelir
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demiroğ";
+            int dogumYili = 1985;
+            long tcNo = 12345678910;
+
+
+            Console.WriteLine(tutar * 1.18);
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+
+
+    // PascalCasing  -  Class'lar, property'ler, metodlar PascalCase yazılır.
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+
+    }
+}
